6bc68d6ece068b3ee092ce317911ed4358ba64d0
bee7er <contact_bee@yahoo.com>
2023-09-22 09:10:47 +0000

    Use rsync to copy files rather than cp

--- srscopy.sh
+++ srscopy.sh
@@ -2,7 +2,9 @@
 
 echo "Running copy to Code"
 
-cp -R /Users/brianetheridge/Library/Preferences/MAXON/Cinema\ 4D\ R20_7DE41E5A/plugins/* /Users/brianetheridge/Code/srsplugin/
+# cp -R /Users/brianetheridge/Library/Preferences/MAXON/Cinema\ 4D\ R20_7DE41E5A/plugins/py-srs_plugin/* /Users/brianetheridge/Code/srsplugin/py-srs_plugin/
+
+rsync -av /Users/brianetheridge/Library/Preferences/MAXON/Cinema\ 4D\ R20_7DE41E5A/plugins/py-srs_plugin /Users/brianetheridge/Code/srsplugin --exclude projects
 
 echo "Done"
 
